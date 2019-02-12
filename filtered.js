b4d6a9376cf1c54f7107e7b365ff56f28a868d34
Ruben de la Fuente <rudelafuente@gmail.com>
2019-02-12 21:08:21 +0100

    Filtered LS

---
+++ filtered.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var path = require('path')
+
+var dir = process.argv[2]
+
+var filter = process.argv[3]
+
+fs.readdir(dir, function (err,list){
+    var total = 0
+    for(var i =0;i<list.length;i++){
+        if(path.extname(list[i]) == '.' + filter)
+            console.log(list[i])
+    }
+})
